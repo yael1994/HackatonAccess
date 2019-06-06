df738742f7523eaf2541f77a9cc516d4ae43df2d
yael mohaban <yael4391@gmail.com>
2019-06-06 07:26:38 +0300

    Add project files.

---
+++ HackatonAccess2/Server.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HackatonAccess2
+{
+    class Server
+    {
+        private TcpListener listener;
+        private TcpClient client;
+        private IPEndPoint ep;
+
+        private BinaryReader reader;
+        private BinaryWriter writer;
+
+        public Server(int port)
+        {
+            ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+
+            listener = new TcpListener(ep);
+            listener.Start();
+
+            client = null;
+        }
+
+        //Blocker till the simulator connected
+        public void Start()
+        {
+            new Thread(() =>
+            {
+              //  Console.WriteLine("Waiting for connections...");
+              client = listener.AcceptTcpClient();
+              //   Console.WriteLine("Got new connection");
+              reader = new BinaryReader(client.GetStream());
+                writer = new BinaryWriter(client.GetStream());
+            }).Start();
+        }
+
+        public void Write(byte[] str)
+        {
+            if(client != null)
+            {
+                writer.Write(str);
+            }
+        }
+
+        public string[] Read()
+        {
+            String buffer = "";
+            char c;
+            try
+            {
+                c = reader.ReadChar();
+            }
+            catch
+            {
+                Console.WriteLine("Reading from client failed");
+                Disconnect();
+                return null;
+
+            }
+
+            while (c != '\n')
+            {
+                buffer += c;
+                try
+                {
+                    c = reader.ReadChar();
+                }
+                catch
+                {
+                    Console.WriteLine("Reading from client failed");
+                    Disconnect();
+                    return null;
+                }
+
+            }
+
+            string[] retStr = buffer.Split(',');
+            return retStr;
+
+        }
+        //To close the Accept Blocker
+        public void Disconnect()
+        {
+            listener.Stop();
+
+        }
+        ~Server()
+        {
+            listener.Stop();
+        }
+    }
+}
---
+++ HackatonAccess2/Watch.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+
+using System.Security.Permissions;
+using System.Text;
+using System.Windows.Input;
+
+namespace HackatonAccess2
+{
+    public class Watch : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private string text;
+        public string Text { get { return text; } set { text = value; NotifyPropertyChanged("Text"); } }
+        public void NotifyPropertyChanged(string propName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+        private Server server;
+        private FileSystemWatcher watcher;
+
+
+        public object voice { get; private set; }
+
+        public Watch()
+        {
+            server = new Server(5400);
+            server.Start();
+        }
+
+        [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+        public void Run()
+        {
+
+            // Create a new FileSystemWatcher and set its properties.
+            watcher = new FileSystemWatcher();
+
+            watcher.Path = "C:\\Users\\yael4\\Desktop\\voice";
+
+            // Watch for changes in LastAccess and LastWrite times, and
+            // the renaming of files or directories.
+            watcher.NotifyFilter = NotifyFilters.LastAccess
+                                 | NotifyFilters.LastWrite
+                                 | NotifyFilters.FileName
+                                 | NotifyFilters.DirectoryName;
+
+            // Only watch text files.
+            watcher.Filter = "*.mp3";
+
+            // Add event handlers.
+            watcher.Created += OnChanged;
+
+            // Begin watching.
+            watcher.EnableRaisingEvents = true;
+
+        }
+
+        // Define the event handlers.
+        private void OnChanged(object source, FileSystemEventArgs e)
+        {
+                        
+            ///////////////////////////////////////////////////////////////////////////////
+            // Specify what is done when a file is changed, created, or deleted.
+            Text = e.FullPath;
+            // server.Write(e.FullPath);
+        }
+
+
+        private ICommand _sendCommand;
+        public ICommand SendCommand
+        {
+            get
+            {
+                Console.WriteLine(1);
+                return _sendCommand ?? (_sendCommand = new CommandHandler(() => OnPress()));
+            }
+        }
+        private void OnPress()
+        {
+            byte[] bytesToSend = Encoding.UTF8.GetBytes(Text);
+            byte[] intBytes = BitConverter.GetBytes(bytesToSend.Length);
+            server.Write(intBytes);
+            server.Write(bytesToSend);
+
+        }
+
+
+    }
+
+}
